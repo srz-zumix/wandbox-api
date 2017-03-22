093d826243fedd3fa5a5cd9e5dfa0064d71d948e
srz_zumix <zumix.cpp@gmail.com>
2017-03-23 08:32:38 +0900

    update api url

--- wandbox/__init__.py
+++ wandbox/__init__.py
@@ -1,7 +1,7 @@
 __author__ = 'Takazumi Shirayanagi'
-__version__ = '0.3.2'
+__version__ = '0.3.3'
 
-__copyright__ = '2014-2016 %s ' % __author__
+__copyright__ = '2014-2017 %s ' % __author__
 __license__ = """
 The MIT License (MIT)
 
