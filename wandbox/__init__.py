828064971175728df89d0875b1699d7134d9c94f
srz_zumix <zumix.cpp@gmail.com>
2020-04-21 23:48:37 +0900

    import

--- wandbox/__init__.py
+++ wandbox/__init__.py
@@ -26,3 +26,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """ % __copyright__
+
+from .wandbox import Wandbox
+from .cli import WandboxCLI
+from .runner import WandboxRunner
