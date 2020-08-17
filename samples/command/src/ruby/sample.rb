f780d6d10fb462cf6e3c8b97679a20f3ad08b53a
srz_zumix <zumix.cpp@gmail.com>
2020-08-17 10:26:58 +0900

    add ruby (#12)
    * add ruby
    
    * fix lint

---
+++ samples/command/src/ruby/sample.rb
@@ -0,0 +1,11 @@
+# This file is a "Hello, world!" in Ruby language by ruby for wandbox.
+
+require './test1'
+require_relative 'test2'
+
+puts "Hello, Wandbox!"
+test1
+test2
+
+# Ruby reference:
+#   https://docs.ruby-lang.org/
