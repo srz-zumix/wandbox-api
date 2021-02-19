9a8648eb6607c3823e2e0ccadf99d2a7ac721dae
srz_zumix <zumix.cpp@gmail.com>
2021-02-19 10:56:14 +0900

    fix dub

--- Makefile
+++ Makefile
@@ -6,8 +6,11 @@ defaut: help
 install: ## install self
 	python setup.py install
 
-test: ## test
+test: ## commands test
 	make -C samples/command
+
+pytest: ## python test
+	python setup.py test	
 
 help: ## Display this help screen
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-20s\033[0m %s\n", $$1, $$2}'
--- wandbox/__dub__.py
+++ wandbox/__dub__.py
@@ -32,6 +32,23 @@ class DubCLI(CLI):
             help='dryrun'
         )
 
+        subparser = self.parser.add_subparsers()
+        run_cmd = subparser.add_parser(
+            'run',
+            prefix_chars='+',
+            description='build and run command',
+            help='build and run command. see `run +h`'
+        )
+        passthrough_cmds = [run_cmd]
+        for passthrough_cmd in passthrough_cmds:
+            passthrough_cmd.set_defaults(handler=self.command_run)
+            passthrough_cmd.add_argument(
+                'options',
+                metavar='OPTIONS',
+                nargs='*',
+                help='options'
+            )
+
     def parse_command_line(self, argv):
         opts, args = self.parser.parse_known_args(argv)
         if 'WANDBOX_DRYRUN' in os.environ:
@@ -43,6 +60,12 @@ class DubCLI(CLI):
 
     def execute_with_args(self, args=None):
         opts, args = self.parse_command_line(args)
+        if hasattr(opts, 'handler'):
+            opts.handler(opts, args)
+        else:
+            self.print_help()
+
+    def command_run(self, opts, args):
         cmd = DubCLI.InnerCLI(opts.compiler)
         run_options = ['run']
         cli_options = args
