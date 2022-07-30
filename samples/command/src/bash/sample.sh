df2d9df0e57325310b301102cac82ac98910231a
srz_zumix <zumix.cpp@gmail.com>
2022-07-30 17:50:20 +0900

    Squashed commit of the following:
    commit 4be1e891706d4fa1a15dce92dfb55fa5b930426e
    Author: srz_zumix <zumix.cpp@gmail.com>
    Date:   Sat Jul 30 17:43:54 2022 +0900
    
        up
    
    commit e06e6dd4a44fbf63b9436056785a7a75b84e88d9
    Author: srz_zumix <zumix.cpp@gmail.com>
    Date:   Sat Jul 30 17:38:50 2022 +0900
    
        fix lint
    
    commit 80260a9ddf9d2576b414a5e8136e8b46e9c93d3e
    Author: srz_zumix <zumix.cpp@gmail.com>
    Date:   Sat Jul 30 17:31:21 2022 +0900
    
        fix
    
    commit 783447868716cab752552db93ab10de0594c661e
    Author: srz_zumix <zumix.cpp@gmail.com>
    Date:   Sat Jul 30 17:07:32 2022 +0900
    
        fix shellcheck
    
    commit cc0f51ab3dc40b42e4b5a16f3601a680b2686673
    Author: srz_zumix <zumix.cpp@gmail.com>
    Date:   Sat Jul 30 16:58:48 2022 +0900
    
        fix bash parse

--- samples/command/src/bash/sample.sh
+++ samples/command/src/bash/sample.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 # This file is a "Hello, world!" in Bash script for wandbox.
+
 echo Hello, Wandbox!
 
+# shellcheck source=/dev/null
 source test1.sh
-. test2.sh
-. ./test2.sh
+# shellcheck source=/dev/null
+. test2-1.sh
+# shellcheck source=/dev/null
+. ./test2-2.sh;
 
 # ./test3.sh
-echo ${SHELL}
-${SHELL} ./test3.sh
+echo "${SHELL}"
+"${SHELL}" ./test3.sh
 sh ./test3.sh
 bash ./test3.sh
 
@@ -16,7 +20,12 @@ bash ./test3.sh
 #   ls /opt/wandbox
 # fi
 
-echo $*
+# shellcheck source=/dev/null
+. "test 4;.sh";
+# shellcheck source=/dev/null
+source "test 4.sh"; . ./test4.sh;source "./test 4;;.sh"
+
+echo "$@"
 
 if [ "$1" != "local" ]; then
     if [ "$1" != "wandbox" ]; then
