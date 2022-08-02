b9817135c94b64c1c3d3b9614fbaf6cee6d04c09
srz_zumix <zumix.cpp@gmail.com>
2022-08-02 23:50:33 +0900

    support php statement

--- samples/command/src/php/sample.php
+++ samples/command/src/php/sample.php
@@ -2,8 +2,7 @@
 // This file is a "Hello, world!" in PHP for wandbox.
 print("Hello, Wandbox!\n");
 
-include 'test1.php';
-require "test2.php";
+include 'test1.php'; require "test2.php";
 include_once "subdir/test3.php";
 require_once "subdir/test4.php";
 
