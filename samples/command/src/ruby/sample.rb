6c0f7ba9a73539b46107d13931b7f3159451a423
srz_zumix <zumix.cpp@gmail.com>
2022-08-03 22:14:54 +0900

    support Ruby statement

--- samples/command/src/ruby/sample.rb
+++ samples/command/src/ruby/sample.rb
@@ -1,11 +1,12 @@
 # This file is a "Hello, world!" in Ruby language by ruby for wandbox.
 
 require './test1'
-require_relative 'test2'
+require_relative 'test2'; require './test3'
 
 puts "Hello, Wandbox!"
 test1
 test2
+test3
 
 # Ruby reference:
 #   https://docs.ruby-lang.org/
