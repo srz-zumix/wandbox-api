44015670aa56ed0b04d64f347c50d07e90f3c9da
srz_zumix <zumix.cpp@gmail.com>
2022-08-03 22:52:53 +0900

    update sample code

--- samples/command/src/python/sample.py
+++ samples/command/src/python/sample.py
@@ -1,8 +1,7 @@
 # This file is a "Hello, world!" in Python language by CPython for wandbox.
 
 import test, test4
-from test2 import hoge
-from test2 import hoge2, hoge3
+from test2 import hoge; from test2 import hoge2, hoge3
 from test3 import fuga as FUGA
 from subdir.test5 import test as Test
 
--- samples/command/src/scala/sample.scala
+++ samples/command/src/scala/sample.scala
@@ -1,8 +1,7 @@
 // This file is a "Hello, world!" in Scala language for wandbox.
 import scala.language.postfixOps
 import test1._
-import _root_.test3.Test3, test4.Test4
-import test4.{Test05 => Test5}
+import _root_.test3.Test3, test4.Test4; import test4.{Test05 => Test5}
 
 object Wandbox {
   def main(args: Array[String]): Unit = {
